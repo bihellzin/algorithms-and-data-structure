700a9f87173000ce38210094ee1f9e7ab0f34ceb
bihellzin <gof2@cin.ufpe.br>
2019-09-12 11:55:50 -0300

    adding searching and removing methods

--- list-stack-queue/singly-linked-list.py
+++ list-stack-queue/singly-linked-list.py
@@ -1,8 +1,9 @@
 class Node(object):
 
-    def __init__(self, data, next = None):
+    def __init__(self, data, nxt=None):
         self.data = data
-        self.next = next
+        self.nxt = nxt
+
 
 class LinkedList(object):
 
@@ -10,46 +11,116 @@ class LinkedList(object):
         self.root = None
         self.size = 0
 
-    def getSize(self):
+    def emptyList(self):
+        if self.root is None:
+            return True
+        else:
+            return False
+
+    def get_size(self):
         return self.size
 
     def insertEnd(self, newNode):
-        if self.root is None:
+        if self.emptyList():
             self.root = newNode
 
         else:
             lastNode = self.root
 
             while True:
-                if lastNode.next is None:
+                if lastNode.nxt is None:
                     break
-                lastNode = lastNode.next
-            lastNode.next = newNode
+                lastNode = lastNode.nxt
+            lastNode.nxt = newNode
         self.size += 1
 
     def insertBegin(self, newNode):
-        temp = self.root
-        self.root = newNode
-        self.root.next = temp
-        self.size += 1
+        if self.emptyList():
+            self.root = newNode
+
+        else:
+            temp = self.root
+            self.root = newNode
+            self.root.nxt = temp
+            self.size += 1
 
     def insertSorted(self, newNode):
-        if newNode.next is None:
+        if newNode.nxt is None:
             self.insertEnd(newNode)
             return
 
         else:
             lastNode = self.root
-            temp = self.root.next
+            temp = self.root.nxt
             while True:
-                if temp.data is newNode.next or temp.next is None:
+                if temp.data is newNode.nxt or temp.nxt is None:
                     break
                 lastNode = temp
-                temp = lastNode.next
-            lastNode.next = newNode
-            newNode.next = temp
+                temp = lastNode.nxt
+            lastNode.nxt = newNode
+            newNode.nxt = temp
 
+    def removeBegin(self):
+        if self.emptyList():
+            print('The list is empty')
+            return
 
+        else:
+            self.root = self.root.nxt
+
+    def removeEnd(self):
+        if self.emptyList():
+            print('The list is empty')
+            return
+
+        else:
+            lastNode = self.root
+            prev = None
+            while lastNode.nxt is not None:
+                prev = lastNode
+                lastNode = lastNode.nxt
+            prev.nxt = None
+
+    def removeNode(self, nodeData):
+        if self.emptyList():
+            print('The list is empty')
+            return
+
+        else:
+            currentNode = self.root
+            prev = None
+            while currentNode.nxt is not None:
+                if currentNode.data == nodeData:
+                    if prev is not None:
+                        prev.nxt = currentNode.nxt
+
+                    else:
+                        self.root = currentNode.nxt
+
+                    return True
+
+                prev = currentNode
+                currentNode = currentNode.nxt
+
+            return False
+
+    def searchNode(self, nodeData):
+        if self.emptyList():
+            print('The list is empty')
+            return
+
+        else:
+            currentNode = self.root
+            while currentNode.nxt is not None and currentNode.data != nodeData:
+                currentNode = currentNode.nxt
+
+            if currentNode.data == nodeData:
+                print('The node you are looking for', currentNode.data, currentNode)
+                return True
+
+            elif currentNode.nxt is None:
+                print('The node you want to remove does not exist')
+                return False
 
     def printList(self):
         if self.root is None:
@@ -60,22 +131,33 @@ class LinkedList(object):
             if currentNode is None:
                 break
             print(currentNode.data)
-            currentNode = currentNode.next
+            currentNode = currentNode.nxt
+
 
 def main():
-    list = LinkedList()
+    myList = LinkedList()
+
     fn = Node('John')
-    list.insertEnd(fn)
-    sn = Node('Ben')
-    list.insertEnd(sn)
+    myList.insertEnd(fn)
+
     tn = Node('Matthew')
-    list.insertEnd(tn)
+    myList.insertEnd(tn)
+
+    sn = Node('Ben')
+    myList.insertEnd(sn)
 
     qn = Node('Gabriel')
-    list.insertBegin(qn)
+    myList.insertBegin(qn)
 
     an = Node('Natan', 'Ben')
-    list.insertSorted(an)
-    list.printList()
+    myList.insertSorted(an)
 
-main() 
+    myList.printList()
+    print('')
+    myList.removeEnd()
+    myList.printList()
+    print('')
+    print(myList.root.data)
+
+
+main()

08020077a40457e8971162ea0abb234baa8f06f3
bihellzinho <gof2@cin.ufpe.br>
2019-09-17 14:36:20 -0300

    adding a removing method (incomplete)

--- trees/binary-search-tree.py
+++ trees/binary-search-tree.py
@@ -42,39 +42,74 @@ class BinaryTree:
         while True:
             if value > node.value:
                 if node.value == value:
-                    return value
+                    return node
 
                 elif node.right is None:
                     print('Value not found')
-                    return
+                    return False
 
                 node = node.right
 
             else:
                 if node.value == value:
-                    return value
+                    return node
 
                 elif node.left is None:
                     print('Value not found')
-                    return
+                    return False
 
                 node = node.left
 
+    def remove(self, value):
+        node = self.search(value)
+        if node is not False:
+            if node.right is None and node.left is None:
+                if node.parent.right == node:
+                    node.parent.right = None
+                    node.parent = None
+                else:
+                    node.parent.left = None
+                    node.parent = None
+
+            elif node.right is not None and node.left is not None:
+                pass
+
+            elif node.right is None and node.left is not None:
+                if node.value > node.parent.value:
+                    node.parent.right = node.left
+                    node.parent = None
+
+                else:
+                    node.parent.left = node.left
+                    node.parent = None
+
+            elif node.right is not None and node.left is None:
+                if node.value > node.parent.value:
+                    node.parent.right = node.right
+                    node.parent = None
+
+                else:
+                    node.parent.left = node.right
+                    node.parent = None
+
+        else:
+            return False
+        
 if __name__ == '__main__':
     arvre = BinaryTree()
     no2 = Node(2)
     no4 = Node(4)
     no1 = Node(1)
     no3 = Node(3)
-    no6 = Node(6)
-    no8 = Node(8)
-    no = Node(3)
+    #no6 = Node(6)
+    #no8 = Node(8)
+
 
     arvre.insert(no2)
     arvre.insert(no4)
     arvre.insert(no1)
     arvre.insert(no3)
-    arvre.insert(no6)
-    arvre.insert(no8)
-    arvre.insert(no)
-    print(arvre.root.right.left.right)
+    #arvre.insert(no6)
+    #arvre.insert(no8)
+    arvre.remove(4)
+    print(arvre.root.right.value)

6ebc1bb841d8a8abf9dc28598b7647dfa4ce6091
Gabriel de Oliveira Ferreira <gof2@WINDOWS.CIN.UFPE.BR>
2019-10-30 17:50:34 -0300

    adding new stuff

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,7 +1,6 @@
 class Vertice:
     def __init__(self, nome):
         self.nome = nome
-        self.peso = 1
         self.vizinhos = []
         self.cor = 'branco'
 
@@ -14,9 +13,27 @@ class Vertice:
 
 class Grafo_Lista:
     def __init__(self, iteravel=None):
-        self.vertices = {}
+        self.ponderado = False
         if iteravel is None:
-            pass
+            self.vertices = {}
+
+        else:
+            self.vertices = {}
+            if len(iteravel[0]) > 2:
+                self.ponderado = True
+            for i in range(len(iteravel)):
+
+                for j in range(1):
+                    if self.ponderado is False:
+                        self.add_vertice(Vertice(iteravel[i][0]))
+                        self.add_vertice(Vertice(iteravel[i][1]))
+                        self.add_aresta(iteravel[i][0], iteravel[i][1])
+
+                    else:
+                        self.add_vertice(Vertice(iteravel[i][0]))
+                        self.add_vertice(Vertice(iteravel[i][1]))
+                        self.add_aresta(iteravel[i][0], iteravel[i][1], iteravel[i][2])
+
 
     def add_vertice(self, vertice):
         if isinstance(vertice, Vertice) and vertice.nome not in self.vertices:
@@ -26,14 +43,23 @@ class Grafo_Lista:
         else:
             return False
 
-    def add_aresta(self, v1, v2):
+    def add_aresta(self, v1, v2, peso=1):
         if v1 in self.vertices and v2 in self.vertices:
-            for key, value in self.vertices.items():
-                if key == v1:
-                    value.add_vizinho(v2)
+            if self.ponderado is False:
+                for key, value in self.vertices.items():
+                    if key == v1:
+                        value.add_vizinho(v2)
 
-                if key == v2:
-                    value.add_vizinho(v1)
+                    if key == v2:
+                        value.add_vizinho(v1)
+
+            else:
+                for key, value in self.vertices.items():
+                    if key == v1:
+                        value.add_vizinho((v2, peso))
+
+                    if key == v2:
+                        value.add_vizinho((v1, peso))
 
             return True
 
@@ -46,23 +72,28 @@ class Grafo_Lista:
         return False
 
     def grau_saida(self, vertice):
-        return len(self.vertices[vertice].vizinhos)
+        #return len(self.vertices[vertice].vizinhos)
+        pass
 
     def grau_entrada(self, vertice):
-        entrada = 0
+        '''entrada = 0
         for i in self.vertices:
             if vertice in self.vertices[i].vizinhos:
                 entrada += 1
 
-        return entrada
+        return entrada'''
+        pass
 
     def adjacentes(self, vertice):
         return self.vertices[vertice].vizinhos
 
     def __str__(self):
         retorno = ''
-        for key in sorted(list(self.vertices.keys())):
-            retorno += str(key) + str(self.vertices[key].vizinhos) + '\n'
+        if self.ponderado is False:
+            for key in sorted(list(self.vertices.keys())):
+                retorno += str(key) + str(self.vertices[key].vizinhos) + '\n'
+
+        else:
 
         return retorno
 
